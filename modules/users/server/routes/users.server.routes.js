72de5340011f2ad797fb75f4f7ae8e5711c4d874
kanomdook <kanung.2535@gmail.com>
2017-10-27 16:45:19 +0700

    make it better

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -13,5 +13,5 @@ module.exports = function (app) {
   app.route('/api/users/picture').post(users.changeProfilePicture);
 
   // Finish by binding the user middleware
-  app.param('userId', users.userByID);
+  // app.param('userId', users.userByID);
 };
