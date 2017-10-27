e1fa131ce2baaa16db916c298fb85e383dd41e4b
kanomdook <kanung.2535@gmail.com>
2017-10-27 16:22:34 +0700

    make it better

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -2,10 +2,11 @@
 
 module.exports = function (app) {
   // User Routes
-  var users = require('../controllers/users.server.controller');
+  var core = require('../../../core/server/controllers/core.server.controller'),
+    users = require('../controllers/users.server.controller');
 
   // Setting up the users profile api
-  app.route('/api/users/me').get(users.me);
+  app.route('/api/users/me').post(core.requiresLoginToken, users.me);
   app.route('/api/users').put(users.update);
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
