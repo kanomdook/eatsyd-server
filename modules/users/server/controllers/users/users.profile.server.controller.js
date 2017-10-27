1695d34f0676549ae2b305f4f5b973eb77f76c1d
kanomdook <kanung.2535@gmail.com>
2017-10-27 16:40:35 +0700

    make it better

--- modules/users/server/controllers/users/users.profile.server.controller.js
+++ modules/users/server/controllers/users/users.profile.server.controller.js
@@ -99,5 +99,7 @@ exports.changeProfilePicture = function (req, res) {
  * Send User
  */
 exports.me = function (req, res) {
+  console.log('=========================Apple=========================');
   res.json(req.user);
+  console.log('=======================================================');  
 };
