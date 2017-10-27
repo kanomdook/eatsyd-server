c1b3a4667b6351eed80802e664c20be83b2fa877
kanomdook <kanung.2535@gmail.com>
2017-10-27 19:02:46 +0700

    make it better

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
 
   // Setting up the users profile api
   app.route('/api/users/me').get(core.requiresLoginToken, users.me);
+  app.route('/api/users/me').post(core.requiresLoginToken, users.me);
   app.route('/api/users').put(users.update);
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
