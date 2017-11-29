6bd373c196554074db47f3ae11f1c39a65ba28f1
nicknameismos <mynameissarawut@gmail.com>
2017-11-29 20:55:18 +0700

    update

--- modules/categoryproducts/server/routes/categoryproducts.server.routes.js
+++ modules/categoryproducts/server/routes/categoryproducts.server.routes.js
@@ -4,16 +4,21 @@
  * Module dependencies
  */
 var categoryproductsPolicy = require('../policies/categoryproducts.server.policy'),
+  core = require('../../../core/server/controllers/core.server.controller'),
   categoryproducts = require('../controllers/categoryproducts.server.controller');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Categoryproducts Routes
-  app.route('/api/categoryproducts').all(categoryproductsPolicy.isAllowed)
-    .get(categoryproducts.list)
+  app.route('/api/categoryproducts') //.all(core.requiresLoginToken, categoryproductsPolicy.isAllowed)
+    .get(categoryproducts.list);
+
+  app.route('/api/categoryproducts').all(core.requiresLoginToken, categoryproductsPolicy.isAllowed)
     .post(categoryproducts.create);
 
-  app.route('/api/categoryproducts/:categoryproductId').all(categoryproductsPolicy.isAllowed)
-    .get(categoryproducts.read)
+  app.route('/api/categoryproducts/:categoryproductId') //.all(core.requiresLoginToken, categoryproductsPolicy.isAllowed)
+    .get(categoryproducts.read);
+
+  app.route('/api/categoryproducts/:categoryproductId').all(core.requiresLoginToken, categoryproductsPolicy.isAllowed)
     .put(categoryproducts.update)
     .delete(categoryproducts.delete);
 
