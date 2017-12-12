e6d458a53a9c4709db0ab1c3773a32016ea71375
nicknameismos <mynameissarawut@gmail.com>
2017-12-12 18:33:14 +0700

    update

--- modules/shops/server/controllers/shops.server.controller.js
+++ modules/shops/server/controllers/shops.server.controller.js
@@ -417,8 +417,8 @@ exports.sortName = function (req, res, next) {
   var firstIndex = 0;
   var lastIndex = 10;
   if (req.body.currentpage > 1) {
-    firstIndex = (req.body.currentpage - 1) * 10;
-    lastIndex = (req.body.currentpage * 10) - 1;
+    firstIndex = ((req.body.currentpage - 1) * 10);
+    lastIndex = (req.body.currentpage * 10);
   }
   if (req.body.typename === 'รายการร้านค้า') {
     var numpage = [];
