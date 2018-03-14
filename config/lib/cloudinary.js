d2b736983d1c970c26fa95aadd24062af9ea80c7
bu4y <amnes1a@windowslive.com>
2018-03-14 11:23:20 +0700

    update

--- config/lib/cloudinary.js
+++ config/lib/cloudinary.js
@@ -2,10 +2,17 @@
 var cloudinary = require('cloudinary');
 
 // cloudinary purchasetest
+// cloudinary.config({
+//     cloud_name: 'ha7s6qhcg',
+//     api_key: '979335999988114',
+//     api_secret: 'nbgYN7gcr0PnbBPjkAFPYqGA7yU'
+// });
+
+// cloudinary eatsyd-test
 cloudinary.config({
-    cloud_name: 'ha7s6qhcg',
-    api_key: '979335999988114',
-    api_secret: 'nbgYN7gcr0PnbBPjkAFPYqGA7yU'
+    cloud_name: 'hhb4v5ds8',
+    api_key: '388512427276879',
+    api_secret: 'E7nqKZdC1SmmRnU-nzE17pj9HVM'
 });
 
 module.exports.cloudinary = cloudinary;
